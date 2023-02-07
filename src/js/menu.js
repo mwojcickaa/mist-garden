364bba888f1314966571a247d83e6958d025cd68
mwojcickaa <martynawojcickaaa@gmail.com>
2023-02-07 17:45:31 +0100

    Make sticky menu ikon

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,14 +1,16 @@
 function menu() {
     let menuLinks = document.getElementById("menu")
-    // let animateMenu = document.getElementById("hamburger-menu")
+    let stickyIkon = document.getElementById("hamburger-ikon")
     if (menuLinks.style.display === "block") {
         menuLinks.classList.replace("menu-open", "menu-close")
+        stickyIkon.classList.replace("sticky-menu-ikon", "menu-ikon")
         setTimeout(function () {
             menuLinks.style.display = "none";
         }, 300)
     } else {
         menuLinks.style.display = "block";
         menuLinks.classList.replace("menu-close", "menu-open")
+        stickyIkon.classList.replace("menu-ikon", "sticky-menu-ikon")
     }
 
 }
