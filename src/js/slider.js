a67f4f014a9be4ea8c90e5e780da0e0f83ee38f7
mwojcickaa <martynawojcickaaa@gmail.com>
2023-02-08 23:54:16 +0100

    Fix the slider and clean the code

--- src/js/slider.js
+++ src/js/slider.js
@@ -2,11 +2,11 @@ let slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-    showSlides(slideIndex += n);
+    showSlides(slideIndex += n)
 }
 
 function currentSlide(n) {
-    showSlides(slideIndex = n);
+    showSlides(slideIndex = n)
 }
 
 function showSlides(n) {
@@ -25,63 +25,7 @@ function showSlides(n) {
     dots[slideIndex - 1].className += " active"
 }
 
-
-
-var timeOut = 3000;
-var autoOn = true;
-
-autoSlides();
-
-function autoSlides() {
-    timeOut = timeOut - 20;
-
-    if (autoOn == true && timeOut < 0) {
-        showSlides();
-    }
-    setTimeout(autoSlides, 20);
-}
-
-function prevSlide() {
-
-    timeOut = 3000;
-
-    var slides = document.getElementsByClassName("background-color-second-section");
-    var dots = document.getElementsByClassName("dot");
-
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slideIndex--;
-
-    if (slideIndex > slides.length) {
-        slideIndex = 1
-    }
-    if (slideIndex == 0) {
-        slideIndex = 3
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-}
-
-function showSlides() {
-
-    timeOut = 3000;
-
-    var slides = document.getElementsByClassName("background-color-second-section");
-    var dots = document.getElementsByClassName("dot");
-
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slideIndex++;
-
-    if (slideIndex > slides.length) {
-        slideIndex = 1
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-}
-
-
+setInterval(() => {
+    slideIndex += 1
+    showSlides(slideIndex)
+}, 3000)
