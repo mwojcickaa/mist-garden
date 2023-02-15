8cf509727bfddf5761dd4bdca0bf1ae02ea23e60
mwojcickaa <martynawojcickaaa@gmail.com>
2023-02-15 22:27:00 +0100

    Fix sticky hamburger icon

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,10 +1,16 @@
 function menu() {
+    // debugger
     let menu = document.getElementById("menu")
     let stickyMenuIcon = document.getElementById("hamburger-icon")
+    let hamburger = document.getElementById("hamburger")
+    let hamburgerInput = document.getElementById("checkbox")
     if (menu.style.display === "block") {
         menu.classList.replace("menu-open", "menu-close")
         stickyMenuIcon.classList.remove("sticky-menu-icon")
         stickyMenuIcon.classList += " menu-close"
+        hamburger.classList.remove("sticky-hamburger")
+        hamburgerInput.classList.remove("sticky-hamburger")
+        hamburgerInput.classList += " input"
         setTimeout(function () {
             menu.style.display = "none"
         }, 300)
@@ -13,6 +19,9 @@ function menu() {
         menu.classList.replace("menu-close", "menu-open")
         stickyMenuIcon.classList.remove("menu-close")
         stickyMenuIcon.classList += " sticky-menu-icon"
+        hamburger.classList += " sticky-hamburger"
+        hamburgerInput.classList.remove("input")
+        hamburgerInput.classList += " sticky-hamburger"
     }
 }
 
